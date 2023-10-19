bf2498fd32ee10c32159db3c85547675fa85782b
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-19 14:51:38 +0800

    add formik: validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -10,6 +10,25 @@ const YoutubeForm = () => {
     },
     onSubmit: values => {
       console.log(values)
+    },
+    validate: values => {
+      let errors = {}
+
+      if(!values.name) {
+        errors.name = 'Required'
+      }
+
+      if(!values.email) {
+        errors.email = 'Required'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email format'
+      }
+
+      if(!values.channel) {
+        errors.channel = 'Required'
+      }
+
+      return errors
     }
   })
 
