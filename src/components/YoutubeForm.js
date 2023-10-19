f7ebaa159ef810c57cdbfb21b2b5dbe882e6b8de
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-19 14:55:12 +0800

    optimize the code

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,35 +1,41 @@
 import { useFormik } from 'formik';
 
+const initialValues = {
+  name: '',
+  email: '',
+  channel: ''
+}
+
+const onSubmit = values => {
+  console.log(values)
+}
+
+const validate = values => {
+  let errors = {}
+
+  if(!values.name) {
+    errors.name = 'Required'
+  }
+
+  if(!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email format'
+  }
+
+  if(!values.channel) {
+    errors.channel = 'Required'
+  }
+
+  return errors
+}
+
 const YoutubeForm = () => {
 
   const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      channel: ''
-    },
-    onSubmit: values => {
-      console.log(values)
-    },
-    validate: values => {
-      let errors = {}
-
-      if(!values.name) {
-        errors.name = 'Required'
-      }
-
-      if(!values.email) {
-        errors.email = 'Required'
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email format'
-      }
-
-      if(!values.channel) {
-        errors.channel = 'Required'
-      }
-
-      return errors
-    }
+    initialValues,
+    onSubmit,
+    validate
   })
 
   return ( 
