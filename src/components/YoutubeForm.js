91376ef9166eb78320ed1b57399169fbdf6466c3
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-20 11:56:55 +0800

    add FieldArray component

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,4 +1,4 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
 import * as Yup from 'yup';
 import TextError from './TextError'
 
@@ -12,7 +12,8 @@ const initialValues = {
     facebook: '',
     twitter: ''
   },
-  phoneNumbers: ['', '']
+  phoneNumbers: ['', ''],
+  phNumbers: ['']
 }
 
 const onSubmit = values => {
@@ -124,6 +125,34 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="secondaryPh">Secondary phone number</label>
           <Field type="text" id="secondaryPh" name="phoneNumbers[1]"/>
+        </div>
+
+        <div className="form-control">
+          <label>List of phone numbers</label>
+          <FieldArray name="phNumbers">
+            {
+              (fieldArrayProps) => {
+                console.log("fieldArrayProps", fieldArrayProps)
+                const { push, remove, form } = fieldArrayProps
+                const { values } = form
+                const { phNumbers } = values
+                return (
+                  <div>
+                    {phNumbers.map((phNumber, index) => (
+                      <div key={index}>
+                        <Field name={`phNumbers[${index}]`}/>
+                        {
+                          index > 0 && 
+                          <button type="button" onClick={() => remove(index)}> - </button>
+                        }
+                        <button type="button" onClick={() => push("")}> + </button>
+                      </div>
+                    ))}
+                  </div>
+                )
+              }
+            }
+          </FieldArray>
         </div>
 
         <button type="submit">Submit</button>
